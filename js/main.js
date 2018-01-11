6cb1defbdcb86583b191779dc552f6cc03f323cb
Katie Angelo <krineangelo@gmail.com>
2018-01-11 02:35:58 -0500

    Finished HTML, completed basic CSS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* ----------------------------------------
+Buttons
+------------------------------------------- */
+$('#read-more-problem').on('click', function () {
+     window.location.href="problem.html"
+  });
+$('#read-more-solution').on('click', function () {
+     window.location.href="solution.html"
+  });
+$('#read-more-about').on('click', function () {
+     window.location.href="about.html"
+  });
